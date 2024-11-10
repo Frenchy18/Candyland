7ec99cf9c6aa04f4be95dd9884bbfa88ded5950f
Vincent Baccari <baccarivincent@gmail.com>
2024-11-10 17:02:01 -0500

    updated node class with event variable and message

--- src/main/java/com/example/candyland/DoublyLinkedList.java
+++ src/main/java/com/example/candyland/DoublyLinkedList.java
@@ -1,3 +1,8 @@
+package src.main.java.com.example.candyland;
+
+// create linked list size
+
+
 public class DoublyLinkedList {
     Node head;
     Node tail;
@@ -5,20 +10,19 @@ public class DoublyLinkedList {
     public DoublyLinkedList(){
         this.head = null;
         this.tail = null;
-
     }
+
     public void moveForward(){
         Node current = head;
         while(current != null){
             current = current.next;
         }
-
     }
+
     public void moveBackwards(){
         Node current = tail;
         while (current != null){
             current = current.prev;
         }
     }
-
 }
--- src/main/java/com/example/candyland/Node.java
+++ src/main/java/com/example/candyland/Node.java
@@ -1,11 +1,22 @@
+package src.main.java.com.example.candyland;
+
+
+// add event variable
 public class Node {
-    int data;
+    int event;
+    String eventMessage;
+    int spaceNumber;
+    String spaceColor;
     Node prev;
     Node next;
 
-    public Node(int data){
-        this.data = data;
+    public Node(int spaceNumber,String spaceColor, int event, String eventMessage){
+        this.event = event;
+        this.eventMessage = eventMessage;
+        this.spaceColor = spaceColor;
+        this.spaceNumber = spaceNumber;
         this.prev = null;
         this.next = null;
     }
+
 }
