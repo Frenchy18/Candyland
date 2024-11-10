935db7b23b5c0c458a21d0dddd73f3b3032353f6
Vincent Baccari <baccarivincent@gmail.com>
2024-11-10 15:44:08 -0500

    basic node and linked list class

---
+++ DoublyLinkedList.java
@@ -0,0 +1,24 @@
+public class DoublyLinkedList {
+    Node head;
+    Node tail;
+
+    public DoublyLinkedList(){
+        this.head = null;
+        this.tail = null;
+
+    }
+    public void moveForward(){
+        Node current = head;
+        while(current != null){
+            current = current.next;
+        }
+
+    }
+    public void moveBackwards(){
+        Node current = tail;
+        while (current != null){
+            current = current.prev;
+        }
+    }
+
+}
