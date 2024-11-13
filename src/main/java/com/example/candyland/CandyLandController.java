16c5a2fbf25dd2c8407e984c4a27606a5719c62d
Frenchy18 <chris.grover9292@gmail.com>
2024-11-13 13:40:23 -0500

    Added Candyland Controller File

---
+++ src/main/java/com/example/candyland/CandyLandController.java
@@ -0,0 +1,12 @@
+package com.example.candyland;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class CandyLandController {
+
+    @FXML
+    protected void buttonEndGame() {
+        System.exit(0);
+    }
+}
